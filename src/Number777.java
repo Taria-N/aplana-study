22adfde21ee18ef8b61e32792b98ea98da891d7a
taria-n <natalia.verboom@yandex.ru>
2019-02-25 23:24:10 +0300

    задание 5

---
+++ src/Number777.java
@@ -0,0 +1,15 @@
+public class Number777 {
+    public static void main(String[] args) {
+        int b = 7;
+        System.out.println(b*1);
+        System.out.println(b*2);
+        System.out.println(b*3);
+        System.out.println(b*4);
+        System.out.println(b*5);
+        System.out.println(b*6);
+        System.out.println(b*7);
+        System.out.println(b*8);
+        System.out.println(b*9);
+        System.out.println(b*10);
+    }
+}
