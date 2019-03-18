61201670c6f9c63589329be9c68121cbb959c276
taria-n <natalia.verboom@yandex.ru>
2019-03-18 22:37:16 +0300

    задание 14

---
+++ src/Task14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task14 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String S = scan.nextLine();
+        int X = Integer.parseInt(S);
+        double Y = X;
+        System.out.println(S);
+        System.out.println(X);
+        System.out.println(Y);
+
+    }
+}
